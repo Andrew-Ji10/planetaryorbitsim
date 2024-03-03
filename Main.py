1e0d351c02c878f06b2de65d751c9224ca565ac8
Andrew Ji <jiandrew10@gmail.com>
2024-03-03 00:27:24 -0800

    small organizational changes

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import utils
 
 # in order to solve the lost in space problem, we must be able to do a few things:
 # 1. Identify the locations of the planets (image recognition)
@@ -8,23 +9,6 @@ import matplotlib.pyplot as plt
 
 # 2 -> Developing a model of our solar system using python
 
-G = 6.6743E-11
 
-#calculates the distance between 2 points in 3 dimensions
-def distance(p1, p2):
-    vect = p2 - p1
-    return np.linalg.norm(vect)
-
-#calculates the gravitational acceleration vectors (np arrays) given position and mass
-def gravaccel(p1, p2, m1, m2):
-    dist = distance(p1, p2)
-    unitVect = (p2-p1)/dist
-    fgrav = (G * m1 * m2)/(dist*dist)
-    print(fgrav)
-    accel1 = (fgrav/m1) * unitVect
-    accel2 = (fgrav/m2) * unitVect * -1
-    return accel1, accel2
-
-
-
-print(gravaccel(np.array([0,0,0]), np.array([6378100,0,0]), 100, 5.97219E+24))
+# TODO generate ploting info
+print(utils.gravaccel(np.array([0,0,0]), np.array([6378100,0,0]), 100, 5.97219E+24))
---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#important constants:
+G = 6.6743E-11
+
+#calculates the distance between 2 points in 3 dimensions
+def distance(p1, p2):
+    vect = p2 - p1
+    return np.linalg.norm(vect)
+
+#calculates the gravitational acceleration vectors (np arrays) given position and mass
+def gravaccel(p1, p2, m1, m2):
+    dist = distance(p1, p2)
+    unitVect = (p2-p1)/dist
+    fgrav = (G * m1 * m2)/(dist*dist)
+    print(fgrav)
+    accel1 = (fgrav/m1) * unitVect
+    accel2 = (fgrav/m2) * unitVect * -1
+    return accel1, accel2
